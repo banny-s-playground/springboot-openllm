6a711a20f4719c1e24680d7f366a56f35c03a138
BanseokSuh <still3028@gmail.com>
2025-04-19 17:02:07 +0900

    [main] init: first commit

---
+++ src/main/kotlin/com/banny/open_llm/OpenLlmApplication.kt
@@ -0,0 +1,11 @@
+package com.banny.open_llm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenLlmApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenLlmApplication>(*args)
+}
---
+++ src/test/kotlin/com/banny/open_llm/OpenLlmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banny.open_llm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenLlmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
